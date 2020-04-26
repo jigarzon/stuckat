ad11ec606a5cbe6d79c8951fb8be1edd027f2e90
Juan Ignacio Garzón <9467722+jigarzon@users.noreply.github.com>
2020-04-26 09:55:39 -0300

    mysql

--- src-server/src/db/index.js
+++ src-server/src/db/index.js
@@ -1,16 +1,5 @@
-const MongoClient = require('mongodb').MongoClient
-var appConfig = require('../configs').appConfig
-const url = appConfig.db.connection
-// Database Name
-const dbName = appConfig.db.name
-const client = new MongoClient(url, { useNewUrlParser: true, useUnifiedTopology: true })
-var db = async function () {
-  await client.connect()
-  var database = client.db(dbName)
-  return {
-    db: database,
-    User: database.collection('User')
-  }
-}
-module.exports.client = client
-module.exports.db = db
+var mysql = require("mysql");
+var appConfig = require("../configs").appConfig;
+
+var pool  = mysql.createPool(appConfig.db.connection);
+module.exports.db = pool;

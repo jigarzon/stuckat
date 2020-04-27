b0b5d2b7e096761e9f5f8183f48fb8adf91dba30
Juan Ignacio Garzón <9467722+jigarzon@users.noreply.github.com>
2020-04-27 14:23:29 -0300

    token verify enhancement

--- src-server/src/api/cases.js
+++ src-server/src/api/cases.js
@@ -1,11 +1,14 @@
 /* eslint-disable no-unused-vars */
+var ObjectID = require('mongodb').ObjectID
 const stuckedRouter = require("express").Router();
 var db = require("../db");
 stuckedRouter.post("/cases", async (req, res) => {
   var Case = req.db.collection('Case')
-  var userId = req.user.id;
   var caseInfo = req.body;
+  console.log("creating case for user id", req.user._id)
+  var userId = new ObjectID(req.user._id);
   caseInfo.userId = userId;
+  caseInfo.createdAt = new Date()
   var result = await Case.insertOne(caseInfo);
   res.send({ result: "OK", case: result.value });
 });

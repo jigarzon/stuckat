dd70d396ffa96c05d34b1a7ce0240ae6697bfb57
Juan Ignacio Garzón <9467722+jigarzon@users.noreply.github.com>
2020-04-29 07:57:12 -0300

    fix

--- src/boot/vuex-persist.js
+++ src/boot/vuex-persist.js
@@ -5,9 +5,7 @@ import { Cookies } from 'quasar'
 // https://forum.quasar-framework.org/topic/3306/how-to-make-vuex-store-persist/10
 
 export default async ({ ssrContext, store, Vue }) => {
-  const cookies = process.env.SERVER
-    ? Cookies.parseSSR(ssrContext)
-    : Cookies
+  const cookies = process.env.SERVER ? Cookies.parseSSR(ssrContext) : Cookies
 
   // example, will not use vuex local for now, only cookies
   // const vuexLocal = new VuexPersistence({
@@ -15,22 +13,35 @@ export default async ({ ssrContext, store, Vue }) => {
   //   reducer: (state) => ({ navigation: state.navigation }) // only save navigation module
   // })
   const vuexCookie = new VuexPersistence({
-    restoreState: (key) => {
+    restoreState: key => {
       var data = cookies.get(key)
-      if (Vue.prototype.$auth && data && data.general && data.general.accessToken) {
-        Vue.prototype.$auth.setToken({ access_token: data.general.accessToken })
+      if (
+        Vue.prototype.$auth &&
+        data &&
+        data.general &&
+        data.general.accessToken
+      ) {
+        Vue.prototype.$auth.setToken({
+          access_token: data.general.accessToken
+        })
       }
       return data
     },
     key: 'stuckat',
     saveState: (key, state) => {
-      console.log('saving state', JSON.stringify(state))
-      cookies.set(key, state, { path: '/' })
+      var stateToSave = {
+        general: {
+          user: state.general.user,
+          accessToken: state.general.accessToken
+        }
+      }
+      console.log('saving state', stateToSave)
+      cookies.set(key, stateToSave, { path: '/' })
     },
     modules: ['general'], // only save user module
-    filter: (mutation) =>
-      mutation.type === 'general/setUser' || mutation.type === 'general/setAccessToken'
-
+    filter: mutation =>
+      mutation.type === 'general/setUser' ||
+      mutation.type === 'general/setAccessToken'
   })
   vuexCookie.plugin(store)
   // vuexLocal.plugin(store);
